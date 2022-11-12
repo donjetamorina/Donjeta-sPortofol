600e499dfbd79ad31a42e81acb2a101b6e42293e
Donjeta <donjetam33@gmail.com>
2022-11-12 02:55:04 +0100

    2:45

--- script.js
+++ script.js
@@ -1,54 +1,4 @@
-// let home = document.getElementsByClassName('home');
-// let about = document.getElementsByClassName('about');
-// let services = document.getElementsByClassName('services');
-// let portfolio = document.getElementsByClassName('portfolio');
-// let contact = document.getElementsByClassName('contact');
 
-// let showhome = document.getElementsByClassName('showhome');
-// let showabout = document.getElementsByClassName('showabout');
-// let showservices = document.getElementsByClassName('showservices');
-// let showportfolio = document.getElementsByClassName('showportfolio');
-// let showcontact = document.getElementsByClassName('showcontact');
-// let a = document.querySelector('a');
-
-// a.addEventListener('click', ()=>
-// {
-//     if(showhome.style.display==='none')
-//     {
-//         showhome.style.display= 'block';
-//     }
-//     elseif(showabout.style.display==='none')
-//     {
-//         showabout.style.display= 'block';
-//     }
-//     elseif(showservices.style.display==='none')
-//     {
-//         showservices.style.display= 'block';
-//     }
-//     elseif(showportfolio.style.display==='none')
-//     {
-//         showportfolio.style.display= 'block';
-//     }
-//     else(showcontact.style.display==='none')
-//     {
-//         showcontact.style.display= 'block';
-//     }
-// })
-
-// const parent = document.querySelector('.box-container-services');
-
-// parent.addEventListener('click', event->{
-
-//     const current = event.target;
-
-//     const showMore = current.className.includes('load-more-services');
-
-//     if(!showMore) return;
-
-//     const showDivs = event,target.parentNode.querySelector('.load-more-div');
-
-   
-// })
 function ShowMore()
 {
     var seemore = document.getElementById('load-more-div');
@@ -64,3 +14,17 @@ function ShowMore()
 
 
 }
+
+// function sendEmail (){
+//     Email.send({
+//         Host : "smtp.gmail.com",
+//         Username : "donjetam33@gmail.com",
+//         Password : "10.05.2002",
+//         To : 'donjetam33@gmail.com',
+//         From : document.getElementById("email").value,
+//         Subject : "New Contact Form",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
